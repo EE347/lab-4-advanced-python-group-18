a0da70f1ba2389dff193dc53a6f0936348c28a20
Jake van de Beek <j.vandebeek1@universityofgalway.ie>
2024-10-09 15:50:14 +0100

    Task 7

--- task7.py
+++ task7.py
@@ -3,3 +3,18 @@ import numpy as np
 
 x = np.linspace(0, 10, 101)
 print(x)
+
+# Generate sine and cosine waves
+sin_wave = np.sin(x)
+cos_wave = np.cos(x)
+
+# Save the arrays to files
+np.save('/home/pi/ee347/lab-4-advanced-python-group-18/task7_sin.npy', sin_wave)
+np.save('/home/pi/ee347/lab-4-advanced-python-group-18/task7_cos.npy', cos_wave)
+
+# Verifying by loading and printing
+loaded_sin_wave = np.load('/home/pi/ee347/lab-4-advanced-python-group-18/task7_sin.npy')
+loaded_cos_wave = np.load('/home/pi/ee347/lab-4-advanced-python-group-18/task7_cos.npy')
+
+print('Sine wave:', loaded_sin_wave)
+print('Cosine wave:', loaded_cos_wave)
