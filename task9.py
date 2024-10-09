5cc8ffa5b10526cad9cfbee979fd2674e7574856
Jake van de Beek <j.vandebeek1@universityofgalway.ie>
2024-10-09 16:21:27 +0100

    Task 9

--- task9.py
+++ task9.py
@@ -1,3 +1,25 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as ply
+
+data=pd.read_csv('/home/pi/ee347/lab-4-advanced-python-group-18/task9.csv')
+
+data['Date']= pd.to_datetime(data['Date'], format='%d/%m/%Y')
+
+data.set_index('Date', inplace=True)
+
+plt.figure(figsize=(8,6))
+for column in data.columns:
+    plt.plot(data.index, data[column], label=column)
+
+plt.xlabel('Date')
+plt.ylabel('Stock Prices')
+plt.title('Stock prices over time')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+
